b0515b4075b0ec0185e6e68597fb3268aae415e4
ymjid <m.jid.yannis@gmail.com>
2026-05-28 21:01:24 +0200

    feat: portfolio RPG - fix typo on name project

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('portfofio-rpg');
+  protected readonly title = signal('portfolio-rpg');
 }
